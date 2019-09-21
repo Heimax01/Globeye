4074d8118e519b4f2b8b231ca7dd30f379010ae1
HEIMAX <heitor_campeao@hotmail.com>
2019-09-20 22:24:59 -0300

    Tela teste 1

--- App.js
+++ App.js
@@ -1,69 +1,92 @@
-import React, { Component } from 'react';
-  import {
-    AppRegistry,
-    Dimensions,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View
-  } from 'react-native';
-  import { RNCamera } from 'react-native-camera';
-  
-  type Props = {};
-  export default class App extends Component<Props> {
-      
-    takePicture = async function() {
-      if (this.camera) {
-        const options = { quality: 0.5, base64: true };
-        const data = await this.camera.takePictureAsync(options)
-        console.log(data.uri);
+import React, {Component} from 'react';
+import { View,Text,StyleSheet,Button,Image } from 'react-native';
+
+export default class Teste extends Component {
+
+
+
+
+    constructor(props){
+
+      super(props);
+
+      this.state = {};
+
+    }
+
+     onPressLearnMore (){
+
+            alert("OI");
+
       }
-    }    
+
+
+
+
+  render(){
+
+    return(
+
+
+      <View style={styles.body}>
+
+      <View style={styles.imagem}>
+       <Image
+          style={{width: 250, height: 250}}
+          source={{uri: 'https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/1974526/910/607/m1/fpnw/wm0/2-.png?1480665804&s=2c5e34b5948af207be24250c9ef2e37e'}}
+        />
+      </View>
+
+
       
-    render() {
-      return (
-        <View style={styles.container}>
-          <RNCamera
-              ref={ref => {
-                this.camera = ref;
-              }}
-              style = {styles.preview}
-              type={RNCamera.Constants.Type.back}
-              flashMode={RNCamera.Constants.FlashMode.on}
-              permissionDialogTitle={'Permission to use camera'}
-              permissionDialogMessage={'We need your permission to use your camera phone'}
-          />
-          <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
-          <TouchableOpacity
-              onPress={this.takePicture.bind(this)}
-              style = {styles.capture}
-         >
-              <Text style={{fontSize: 14}}> SNAP </Text>
-          </TouchableOpacity>
-          </View>
-        </View>
-      );
-    }
+
+
+
+
+    <View style={styles.botao}>
+      <Button
+      onPress={this.onPressLearnMore}
+      title="OI"
+      color="#841584"
+      accessibilityLabel="Aperte aqui"
+      />
+    </View>  
+
+      </View>
+
+    );
+
+
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'black'
-    },
-    preview: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      alignItems: 'center'
-    },
-    capture: {
-      flex: 0,
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      padding: 15,
-      paddingHorizontal: 20,
-      alignSelf: 'center',
-      margin: 20
-    }
-  });
+
+
+
+}
+
+const styles = StyleSheet.create({
+
+
+  body:{
+
+    flex:2,
+    justifyContent:'center',
+    alignItems:'center'
+
+
+  },
+
+  imagem:{
+
+      flex:1,
+      paddingTop:30
+
+  },
+
+  botao:{
+    marginTop:20,
+    width:170,
+    marginBottom:20
+  }
+
+
+});
