2a822985504706d05b7aec1b8ac94900c406dfc8
HEIMAX <heitor_campeao@hotmail.com>
2019-09-21 18:24:31 -0300

    Estrutura câmera

--- App.js
+++ App.js
@@ -1,92 +1,154 @@
-import React, {Component} from 'react';
-import { View,Text,StyleSheet,Button,Image } from 'react-native';
-
-export default class Teste extends Component {
-
-
-
-
-    constructor(props){
-
-      super(props);
-
-      this.state = {};
-
-    }
-
-     onPressLearnMore (){
-
-            alert("OI");
-
+//This is an example code for the camera//
+import React from 'react';
+//import react in our code.
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  ActivityIndicator,
+  PermissionsAndroid,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
+//import all the basic components we are going to use.
+import { CameraKitCameraScreen } from 'react-native-camera-kit';
+//import CameraKitCameraScreen we are going to use.
+ 
+export default class App extends React.Component {
+  state = { isPermitted: false };
+  constructor(props) {
+    super(props);
+  }
+  onPress() {
+    var that = this;
+    if (Platform.OS === 'android') {
+      async function requestCameraPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            {
+              title: 'CameraExample App Camera Permission',
+              message: 'CameraExample App needs access to your camera ',
+            }
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //If CAMERA Permission is granted
+            //Calling the WRITE_EXTERNAL_STORAGE permission function
+            requestExternalWritePermission();
+          } else {
+            alert('CAMERA permission denied');
+          }
+        } catch (err) {
+          alert('Camera permission err', err);
+          console.warn(err);
+        }
       }
-
-
-
-
-  render(){
-
-    return(
-
-
-      <View style={styles.body}>
-
-      <View style={styles.imagem}>
-       <Image
-          style={{width: 250, height: 250}}
-          source={{uri: 'https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/1974526/910/607/m1/fpnw/wm0/2-.png?1480665804&s=2c5e34b5948af207be24250c9ef2e37e'}}
+      async function requestExternalWritePermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+            {
+              title: 'CameraExample App External Storage Write Permission',
+              message:
+                'CameraExample App needs access to Storage data in your SD Card ',
+            }
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //If WRITE_EXTERNAL_STORAGE Permission is granted
+            //Calling the READ_EXTERNAL_STORAGE permission function
+            requestExternalReadPermission();
+          } else {
+            alert('WRITE_EXTERNAL_STORAGE permission denied');
+          }
+        } catch (err) {
+          alert('Write permission err', err);
+          console.warn(err);
+        }
+      }
+      async function requestExternalReadPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+            {
+              title: 'CameraExample App Read Storage Read Permission',
+              message: 'CameraExample App needs access to your SD Card ',
+            }
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //If READ_EXTERNAL_STORAGE Permission is granted
+            //changing the state to re-render and open the camera
+            //in place of activity indicator
+            that.setState({ isPermitted: true });
+          } else {
+            alert('READ_EXTERNAL_STORAGE permission denied');
+          }
+        } catch (err) {
+          alert('Read permission err', err);
+          console.warn(err);
+        }
+      }
+      //Calling the camera permission function
+      requestCameraPermission();
+    } else {
+      this.setState({ isPermitted: true });
+    }
+  }
+  onBottomButtomPressed(event) {
+    const captureImages = JSON.stringify(event.captureImages);
+    if (event.type === 'left') {
+      this.setState({ isPermitted: false });
+    } else {
+      Alert.alert(
+        event.type,
+        captureImages,
+        [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+        { cancelable: false }
+      );
+    }
+  }
+  render() {
+    if (this.state.isPermitted) {
+      return (
+        <CameraKitCameraScreen
+          // Buttons to perform action done and cancel
+          actions={{ rightButtonText: 'Done', leftButtonText: 'Cancel' }}
+          onBottomButtonPressed={event => this.onBottomButtonPressed(event)}
+          flashImages={{
+            // Flash button images
+            on: require('./assets/flashon.png'),
+            off: require('./assets/flashoff.png'),
+            auto: require('./assets/flashauto.png'),
+          }}
+          cameraFlipImage={require('./assets/flip.png')}
+          captureButtonImage={require('./assets/capture.png')}
         />
-      </View>
-
-
-      
-
-
-
-
-    <View style={styles.botao}>
-      <Button
-      onPress={this.onPressLearnMore}
-      title="OI"
-      color="#841584"
-      accessibilityLabel="Aperte aqui"
-      />
-    </View>  
-
-      </View>
-
-    );
-
-
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.onPress.bind(this)}>
+            <Text>Open Camera</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
   }
-
-
-
 }
-
 const styles = StyleSheet.create({
-
-
-  body:{
-
-    flex:2,
-    justifyContent:'center',
-    alignItems:'center'
-
-
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
   },
-
-  imagem:{
-
-      flex:1,
-      paddingTop:30
-
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: 300,
+    marginTop: 16,
   },
-
-  botao:{
-    marginTop:20,
-    width:170,
-    marginBottom:20
-  }
-
-
 });
